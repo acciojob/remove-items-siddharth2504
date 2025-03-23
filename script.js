fc373c106a2df74dc7913952748f342a30b33a84
Siddharth Sisodia <139268988+siddharth2504@users.noreply.github.com>
2025-03-24 00:01:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let select = document.getElementById("colorSelect");
+
+        let remove = document.getElementById("button");
+
+        remove.addEventListener("click", removeValue);
+
+        function removeValue(e){
+            let index = select.selectedIndex;
+            if(index !== -1){
+                select.remove(index);
+            }
+        }
